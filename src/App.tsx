160d4515ed72ea48244e7232b8db1d4f9f674c31
vickytourn1-rgb <vickytourn1@gmail.com>
2026-03-09 18:19:17 -0400

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Vercel is working</h1>
+      <p>If you can see this, your deployment is good.</p>
+    </div>
+  );
+}
