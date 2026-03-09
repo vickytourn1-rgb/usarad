b008ae2fbf72cf4d31b7ccf6feec0fd5a0d24417
vickytourn1-rgb <vickytourn1@gmail.com>
2026-03-09 18:19:36 -0400

    Delete App.tsx

--- App.tsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: "40px", fontFamily: "Arial" }}>
-      <h1>Vercel is working</h1>
-      <p>If you can see this, your deployment is good.</p>
-    </div>
-  );
-}
